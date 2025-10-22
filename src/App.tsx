870d934361fcf5bc2c3554063a06bcdcf5b68bf6
Dias <viniciusmonteirodias5@gmail.com>
2025-10-22 20:50:59 -0300

    instalando fontes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { Route, Routes } from 'react-router';
 import Home from './routes/Home';
 import { BlackHole } from './routes/BlackHole';
 import Componentes from './routes/Componentes';
+import '@fontsource/roboto';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
+
 //TODO: remover css styles e adiconar stylesheets
 export default function App() {
   return (
