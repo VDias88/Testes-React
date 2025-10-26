bf88fba93471625e5132d9cd290c301356af9c06
Dias <viniciusmonteirodias5@gmail.com>
2025-10-26 19:39:19 -0300

    t

--- src/routes/Componentes/index.tsx
+++ src/routes/Componentes/index.tsx
@@ -6,8 +6,12 @@ export default function Componentes() {
   return (
     <div className="container">
       <Menu />
-      <div className="container">
+      <div className="section">
         <h1>Página de Components</h1>
+        <p>Aqui temos alguns testes de componentes</p>
+      </div>
+      <div className="section">
+        <h1>Página de Components 2</h1>
         <p>Aqui temos alguns testes de componentes</p>
       </div>
     </div>
